d0cf263d8d993318fa8fee68eed765eb6bf39fd9
le.quang.dao <le.quang.dao@sun-asterisk.com>
2023-07-11 15:58:31 +0700

    Sample normal functions

--- 07_Day_Functions/main.js
+++ 07_Day_Functions/main.js
@@ -4,4 +4,24 @@ function functionName() {
     // code goes here
     console.log('QQQQ')
 }
-functionName()
+
+functionName() // calling function by its name and with parentheses
+
+// function with a parameter & return values
+function sumTwoNumbers(numOne, numTwo) {
+    let sum = numOne + numTwo
+    return sum
+}
+  
+console.log(sumTwoNumbers(10, 20))
+
+// function with unlimited number of parameters
+function sumAllNums() {
+    let sum = 0
+    for (let i = 0; i < arguments.length; i++) {
+      sum += arguments[i]
+    }
+    return sum
+}
+
+console.log(sumAllNums(15, 20, 30, 25, 10, 33, 40))
