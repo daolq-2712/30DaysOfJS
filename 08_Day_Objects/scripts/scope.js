b0f4ecfbfc856682fc29afb741f1fafc9c072d69
le.quang.dao <le.quang.dao@sun-asterisk.com>
2023-07-14 10:07:21 +0700

    Day 8: Local Scope

--- 08_Day_Objects/scripts/scope.js
+++ 08_Day_Objects/scripts/scope.js
@@ -4,12 +4,17 @@ let b = 10 // this is a global scope variable and found in the window object
 
 function letsLearnScope() {
     console.log(a,b)
+    let valid = false
+    // block scope
     if(true) {
         let a = 'Python'
         let b = 100
-        console.log(a,b)
+        let c = 30
+        let d = 40
+        valid = !valid
+        console.log(a,b,c,valid)
     }
-    console.log(a,b)
+    console.log(a,b,valid)
 }
 
 letsLearnScope()
