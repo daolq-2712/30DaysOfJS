bae851cbe0fee83f52613713b2be5b07dbc2c9d9
le.quang.dao <le.quang.dao@sun-asterisk.com>
2023-07-14 11:22:07 +0700

    Day 9: Returning function

--- 09_Day_High_order_functions/scripts/main.js
+++ 09_Day_High_order_functions/scripts/main.js
@@ -9,3 +9,26 @@ function cube(callback, n) {
 }
 
 console.log(cube(callback,3))
+
+const higherOrder = n => {
+    const doSomething = m => {
+        const doWhatEver = t => {
+            return 2*n + 3*m + t
+        }
+        return doWhatEver
+    }
+    return doSomething
+}
+
+console.log(higherOrder(2)(3)(10))
+
+const numbers = [1,2,3,4]
+const sumArray = arr => {
+    let sum = 0
+    arr.forEach(function(element) {
+        sum += element
+    });
+    return sum
+}
+
+console.log(sumArray(numbers))
