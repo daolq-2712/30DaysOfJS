66df76d498afd812996fae5edd1203dc6b37ddee
le.quang.dao <le.quang.dao@sun-asterisk.com>
2023-07-14 09:48:35 +0700

    Day 8: Global scope

--- 08_Day_Objects/scripts/scope.js
+++ 08_Day_Objects/scripts/scope.js
@@ -1,12 +1,15 @@
-// Window Global Object
-a = 'JavaScripts' 
-b = 10 // this is a global scope variable and found in the window object
+// Global scope
+let a = 'JavaScripts' // is a global scope will be found anywhere in this file
+let b = 10 // this is a global scope variable and found in the window object
 
 function letsLearnScope() {
     console.log(a,b)
     if(true) {
+        let a = 'Python'
+        let b = 100
         console.log(a,b)
     }
+    console.log(a,b)
 }
 
 letsLearnScope()
